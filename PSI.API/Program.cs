6a33548916e0fa79c2430045622ef0168fad5a1c
chong <liuhong2003@live.cn>
2022-07-17 01:23:42 +0800

    更新使用 NHibernate 连接 Sqlite

--- PSI.API/Program.cs
+++ PSI.API/Program.cs
@@ -1,8 +1,13 @@
 using PSI.Administration.Identity;
+using PSI.Data;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddNHibernate((config, services) =>
+{
+    config.UseSqlite(services.GetRequiredService<IConfiguration>().GetConnectionString("db"));
+});
 builder.Services.AddIdentity();
 
 WebApplication app = builder.Build();
