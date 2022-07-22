6486b1164f45fb5b0ec4d23626ddb07a2361566b
chong <liuhong2003@live.cn>
2022-07-22 16:56:00 +0800

    添加 users.d.ts

---
+++ ng-ui/src/app/pages/users/users.d.ts
@@ -0,0 +1,17 @@
+interface User {
+  Id: number;
+  UserName: string;
+  NormalizedUserName: string;
+  Email: string;
+  NormalizedEmail: string;
+  EmailConfirmed: boolean;
+  PasswordHash: string;
+  SecurityStamp: string;
+  ConcurrencyStamp: string;
+  PhoneNumber: string;
+  PhoneNumberConfirmed: boolean;
+  TwoFactorEnabled: boolean;
+  LockoutEnd: Date;
+  LockoutEnabled: boolean;
+  AccessFailedCount: number;
+}
