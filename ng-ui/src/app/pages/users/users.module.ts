c29398e0b8b3da2deeb8bcc04892b0860d084afa
chong <liuhong2003@live.cn>
2022-07-23 01:28:36 +0800

    更新 users component 页面

--- ng-ui/src/app/pages/users/users.module.ts
+++ ng-ui/src/app/pages/users/users.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
+import { RoleListComponent } from '../roles/role-list.component';
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    RoleListComponent
   ],
   imports: [
     CommonModule,
