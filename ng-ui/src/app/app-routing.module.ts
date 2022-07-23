690eca0c37f2832982f6b3ecabb4a635aaca967a
chong <liuhong2003@live.cn>
2022-07-24 01:01:16 +0800

    更新 layout component

--- ng-ui/src/app/app-routing.module.ts
+++ ng-ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: '', component: LayoutComponent, }
   //{
   //  path: '', component: LayoutComponent,
   //  children: [
--- ng-ui/src/app/app.module.ts
+++ ng-ui/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
-// import { LayoutComponent } from './layout/layout.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    // LayoutComponent
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
