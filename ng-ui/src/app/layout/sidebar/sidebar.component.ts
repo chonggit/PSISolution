6886fac37c224143f4b7269709e49bdd8ff7b3e2
chong <liuhong2003@live.cn>
2022-07-28 09:14:08 +0800

    页面布局调整

--- ng-ui/src/app/layout/sidebar/sidebar.component.ts
+++ ng-ui/src/app/layout/sidebar/sidebar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 @Component({
-  selector: 'nav[app-sidebar]',
+  selector: 'div[app-sidebar]',
   standalone: true,
   templateUrl: './sidebar.component.html',
   imports: [CommonModule, RouterModule]
